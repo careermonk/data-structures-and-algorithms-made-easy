7f4a0f7c1782d0adfdc2f7ef514608f8e55a8d58
careermonk <karumanchi.narasimha@gmail.com>
2014-12-26 00:46:03 +0530

    Rate Of Growth Test Checin
    Rate Of Growth Test Checin

---
+++ chapter01introduction/ComplexityWithBreakStatement.c
@@ -0,0 +1,29 @@
+//=========================================================================
+// Copyright (c) Dec 22, 2014 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2014-01-10 06:15:46
+// Last modification		: 2008-10-31
+//               by			: Narasimha Karumanchi
+// Book Title				: Data Structures And Algorithms made Easy
+// Warranty         		: This software is provided "as is" without any
+// 				   				warranty; without even the implied warranty of
+// 				    			merchantability or fitness for a particular purpose.
+//=========================================================================
+
+int functionWithBreak(int n){
+	int count = 0;
+	int i = 0,j;
+	for(i=n/2;i<n;i++){
+		j = 1;
+		count++;
+		while (j+n/2 <=n){
+			break;
+			j = j * 2;
+		}
+	}
+    printf("%d\n", count);
+}
+int main(){
+	int n = 20;
+	functionWithBreak(n);
+}
