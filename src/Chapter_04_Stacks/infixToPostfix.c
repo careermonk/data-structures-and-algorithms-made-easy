5c6294facf8dca15dae05d4b901f386a115e488f
CareerMonk <careermonk@users.noreply.github.com>
2019-06-17 11:40:08 +0530

    Update infixToPostfix.c

--- src/Chapter_04_Stacks/infixToPostfix.c
+++ src/Chapter_04_Stacks/infixToPostfix.c
@@ -9,17 +9,16 @@
 
 #include<stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include <limits.h>
 
-struct stack {
+struct Stack {
     int top;
     int capacity;
     int *array;
 };
 
-struct stack *createStack(int capacity) {
-    struct stack *S = malloc(sizeof(struct stack));
+struct Stack *createStack(int capacity) {
+    struct Stack *S = malloc(sizeof(struct Stack));
     if(!S) 
         return NULL;
     S->capacity = capacity;
@@ -30,90 +29,139 @@ struct stack *createStack(int capacity) {
     return S;
 }
 
-int isEmpty(struct stack *S) {
+int isEmpty(struct Stack *S) {
     return (S->top == -1);  	// if the condition is true then 1 is returned else 0 is returned
 }
 
-int isFull(struct stack *S){
+int size(struct Stack *S) {
+    return (S->top + 1);  
+}
+
+int isFull(struct Stack *S){
     //if the condition is true then 1 is returned else 0 is returned
     return (S->top == S->capacity - 1); 
 }
 
-void doubleStack(struct stack *S){
+void doubleStack(struct Stack *S){
     S->capacity *= 2;
     S->array = realloc(S->array, S->capacity * sizeof(int));
 }
 
-void push(struct stack *S, char data){
+
+void push(struct Stack *S, int data){
     if(isFull(S)) 
         doubleStack(S); 
     S->array[++S->top] = data;   
 }
 
-int peek(struct stack *S){
-    /* S->top == - 1 indicates empty stack*/
-    if(isEmpty(S)){	
-        printf("Stack is Empty");
-        return '\0';
-    }
-    else 
-        return (S->array[S->top]); 
-}
-
-char pop(struct stack *S){
+int pop(struct Stack *S){
     /* S->top == - 1 indicates empty stack*/
     if(isEmpty(S)){	
         printf("Stack is Empty\n");
-        return '\0';
+        return INT_MIN;
     }
     else /* Removing element from  ‘top’ of the array and reducing ‘top’ by 1*/
         return (S->array[S->top--]); 
 }
 
-void deleteStack(struct stack *S){
+int peek(struct Stack *S){
+    /* S->top == - 1 indicates empty stack*/
+    if(isEmpty(S)){	
+        printf("Stack is Empty");
+        return INT_MIN;;
+    }
+    else 
+        return (S->array[S->top]); 
+}
+
+void deleteStack(struct Stack *S){
     if(S) {     
         if(S->array)  
              free(S->array);
                   free(S);
       }
 }
+struct AdvancedStack {
+     struct Stack *elementStack;
+     struct Stack *minStack;
+};
 
-int priority(char x){
-    if(x == '(')
-        return 0;
-    if(x == '+' || x == '-')
-        return 1;
-    if(x == '*' || x == '/')
-        return 2;
+
+int isEmptyA(struct AdvancedStack *S) {
+    return (S->elementStack->top == -1);  	// if the condition is true then 1 is returned else 0 is returned
 }
- 
-int infixToPostfix(char expression[]){
-        char *e, x;
-		// create a stack of capacity 5
-		struct stack *stk = createStack(5);
 
-    e = expression;
-    while(*e != '\0'){
-        if(isalnum(*e))
-            printf("%c",*e);
-        else if(*e == '(')
-            push(stk, *e);
-        else if(*e == ')'){
-            while((x = pop(stk)) != '(')
-                printf("%c", x);
-        }
-        else{
-            while(priority(peek(stk)) >= priority(*e))
-                printf("%c",pop(stk));
-            push(stk, *e);
-        }
-        e++;
-    }
-    while(!isEmpty(stk)){
-        printf("%c",pop(stk));
-    }
+int sizeA(struct AdvancedStack *S) {
+    return (S->elementStack->top + 1);  
+}
+
+int isFullA(struct AdvancedStack *S){
+    //if the condition is true then 1 is returned else 0 is returned
+    return (S->elementStack->top == S->elementStack->capacity - 1); 
+}
+
+void pushA(struct AdvancedStack *S, int data){
+    push(S->elementStack, data);
+
+    if(isEmpty(S->minStack) || peek(S->minStack) >= data)
+        push (S->minStack, data);
+    else push (S->minStack, peek(S->minStack));
+}
+
+int popA(struct AdvancedStack *S ){
+    int temp;
+
+    if(isEmpty(S->elementStack)) 
+        return INT_MIN;
+
+    temp = peek(S->elementStack);
+
+    if(peek(S->minStack) == pop(S->elementStack))
+        pop (S->minStack);
+    return temp;
+}
+
+int peekA(struct AdvancedStack *S ){
+    return peek(S->elementStack);
+}
+
+int getMinimum(struct AdvancedStack *S){
+     return peek(S->minStack);
+}
+
+struct AdvancedStack * createAdvancedStack(int capacity){
+     struct AdvancedStack *S = malloc (sizeof (struct AdvancedStack));
+
+     if(!S) 
+          return NULL;
+
+     S->elementStack = createStack(capacity);
+     S->minStack = createStack(capacity);
+     return S;
 }
 
 int main(){
-      infixToPostfix("(a+(b*(d+2)))");
+    int i = 0, capacity = 5;
+    // create a stack of capacity 5
+    struct AdvancedStack *stk = createAdvancedStack(capacity);
+
+    for(i = 0; i <= 2 * capacity; i++){
+        pushA(stk, (7*i)%4);
+    }
+
+    printf("Top element is %d\n", peekA(stk));
+    printf("Stack size is %d\n", sizeA(stk));
+
+    for (i = 0; i <= capacity; i++){
+        printf("Popped element is %d\n", popA(stk));
+        printf("Minimum element is %d\n", getMinimum(stk));
+    }
+
+    if (isEmptyA(stk))
+        printf("Stack is empty");
+    else
+        printf("Stack is not empty");
+
+    deleteStack(stk);
+    return 0;
 }
