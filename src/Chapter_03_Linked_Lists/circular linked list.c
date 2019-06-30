bd0c7ee5217b86e6bc866524df30568aa1e58497
CareerMonk <careermonk@users.noreply.github.com>
2019-06-30 16:30:36 +0530

    Create circular linked list.c

---
+++ src/Chapter_03_Linked_Lists/circular linked list.c
@@ -0,0 +1,109 @@
+/*Copyright (c) 2016 CareerMonk Publications and others.
+#E-Mail            : info@careermonk.com 
+#Creation Date     : 2008-01-10 06:15:46 
+#Created by        : Narasimha Karumanchi 
+#Book Title        : Data Structures And Algorithms Made Easy
+#Warranty          : This software is provided "as is" without any 
+#                    warranty; without even the implied warranty of 
+#                    merchantability or fitness for a particular purpose.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct CLLNode{
+	int data;
+	struct CLLNode* next;
+};
+
+struct CLLNode *head = NULL;
+
+//length of the list
+int length(struct CLLNode *head) {
+    struct CLLNode *current = head;
+    int count = 0;
+    if(head == NULL) 
+        return 0;
+
+    do {      
+        current = current->next;
+        count++;
+    } while (current != head);
+
+    return count;
+}
+
+// display list using iteration
+void printList(struct CLLNode *head) {
+    struct CLLNode *current = head;
+    if(head == NULL) 
+        return;
+
+    do {      
+        printf ("%d-->", current->data);
+        current = current->next;
+    } while(current != head);
+}
+
+// Insert at the beginning of the list 
+void insertAtBeginInCLL (struct CLLNode **head, int data){
+    struct CLLNode *current = *head;
+    struct CLLNode *newNode = (struct CLLNode *) (malloc(sizeof(struct CLLNode)));
+    if(!newNode) { 
+        printf("Memory Error"); 
+        return;
+    }
+    newNode->data = data;
+    newNode->next = newNode;
+    if(current == NULL){
+        *head = newNode;
+        return;
+    }
+    
+    while (current->next != *head) 
+        current = current->next;
+        
+    newNode->next = *head;
+    current->next = newNode;
+    *head = newNode;
+}
+
+// insert item at the end of the list
+void insertAtEndInCLL(struct CLLNode **head, int data){
+    struct CLLNode *current = *head;
+    struct CLLNode *newNode = (struct CLLNode *) (malloc(sizeof(struct CLLNode)));
+    if(!newNode) {
+        printf("Memory Error"); 
+        return;
+    }
+    newNode->data = data;
+    while (current->next != *head) 
+        current = current->next;
+
+    newNode->next = newNode;
+
+    if(*head ==NULL)
+        *head = newNode;
+    else {	
+        newNode->next = *head;
+        current->next = newNode;
+    }
+}
+
+
+
+int main(){
+    struct CLLNode *head = NULL;
+    printf("%d \n",length(head));
+    insertAtBeginInCLL(&head, 30);
+    insertAtBeginInCLL(&head, 20);
+    insertAtBeginInCLL(&head, 10);
+    printf("List length is %d \n",length(head));
+    printList(head);
+    printf("\n");
+    insertAtEndInCLL(&head, 40);
+    insertAtEndInCLL(&head, 50);
+    insertAtEndInCLL(&head, 60);
+    printf("List length is %d \n",length(head));
+    printList(head);
+    return 0;
+}
